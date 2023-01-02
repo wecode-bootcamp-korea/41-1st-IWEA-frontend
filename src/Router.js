818c6115c5b97e9fa3c8f6bb2a264b78d9d00292
jakesungjaekim <codejakecarry@gmail.com>
2023-01-02 20:13:25 +0900

    Modify: 파일구조 수정

--- src/Router.js
+++ src/Router.js
@@ -8,6 +8,7 @@ import ProductDetail from './pages/ProductDetail/ProductDetail';
 import Category from './pages/Category/Category';
 import Cart from './pages/Cart/Cart';
 import Payment from './pages/Payment/Payment';
+import Footer from './components/Footer';
 
 const Router = () => {
   return (
@@ -22,6 +23,7 @@ const Router = () => {
         <Route path="/cart" element={<Cart />} />
         <Route path="/payment" element={<Payment />} />
       </Routes>
+      {/* <Footer />s */}
     </BrowserRouter>
   );
 };
