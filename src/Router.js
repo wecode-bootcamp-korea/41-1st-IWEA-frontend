3074052cd6f649e5df0cc32ff9e8ad1f8ca7bf4b
jakesungjaekim <codejakecarry@gmail.com>
2023-01-02 16:33:28 +0900

    ADD: 초기세팅완료

---
+++ src/Router.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import SignUp from './pages/SignUp/SignUp';
+import Product from './pages/Product/Product';
+import ProductDetail from './pages/ProductDetail/ProductDetail';
+import Category from './pages/Category/Category';
+import Cart from './pages/Cart/Cart';
+import Payment from './pages/Payment/Payment';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/productdetail/:id" element={<ProductDetail />} />
+        <Route path="/category/:product" element={<Category />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/payment" element={<Payment />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+export default Router;
---
+++ src/config/config.js
@@ -0,0 +1,12 @@
+const BASE_URL = '';
+
+const API = {
+  login: `${BASE_URL}/login`,
+  signup: `${BASE_URL}/auth/signUp`,
+  signin: `${BASE_URL}/auth/signIn`,
+  detail: `${BASE_URL}/products/detail`,
+  products: `${BASE_URL}/products/category`,
+  cart: `${BASE_URL}/cart/`,
+};
+
+export { API };
