5021dfe353a9152c418605472b3e169ce823a422
gimcheolho <sm708311@gmail.com>
2023-01-03 15:18:31 +0900

    Add: 회원가입 레이아웃 추가

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,86 +1,215 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SignUp.scss';
 
 const SignUp = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [pw, setPw] = useState('');
+  const [phone, setPhone] = useState('');
+  const [address, setAddress] = useState('');
+
+  const handleLogin = () => {
+    fetch('http://10.58.52.136:3000/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        password: pw,
+        phone: phone,
+        address: address,
+      }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        localStorage.setItem('token', data.accessToken);
+      });
+  };
+
+  function handleNameInput(event) {
+    setName(event.target.value);
+    console.log('name', name);
+  }
+
+  function handleEmailInput(event) {
+    setEmail(event.target.value);
+    console.log('email', email);
+  }
+
+  function handlePwInput(event) {
+    setPw(event.target.value);
+    console.log('pw', pw);
+  }
+
+  function handlePhoneInput(event) {
+    setPhone(event.target.value);
+    console.log('phone', phone);
+  }
+
+  function handleAddressInput(event) {
+    setAddress(event.target.value);
+    console.log('address', address);
+  }
+
   return (
-    <div className="Login">
-      <div className="login-wrap">
-        <div className="login-left">
-          <div className="login-left-inner">
-            <div className="login-header">
-              <img src="images/arrow.png" alt="arrow" className="login-arrow" />
+    <div className="SignUp">
+      <div className="signup-wrap">
+        <div className="signup-left">
+          <div className="signup-left-inner">
+            <div className="signup-header">
+              <img
+                src="images/arrow.png"
+                alt="arrow"
+                className="signup-arrow"
+              />
               <img
                 src="images/loginIWEA.png"
                 alt="IWEA"
-                className="login-logo"
+                className="signup-logo"
               />
             </div>
-            <div className="login-main">
-              <h1 className="login-title">
-                <span>로그인</span>
+            <div className="signup-main">
+              <h1 className="signup-title">
+                <span>회원가입</span>
               </h1>
-              <p className="login-description">
-                외워야 할 비밀번호가 많아 불편하셨죠?
-                <br />
-                이제 일회용 코드를 이용하여 간편하게 로그인하세요.
-                <br />
-                <br />* 이메일 또는 휴대폰 번호 최초 인증 후 사용 가능
-              </p>
+              <div className="signup-description">
+                <span>이미 가입하셨나요?</span>
+                <a href="#" className="link">
+                  로그인 하기
+                </a>
+              </div>
+              <div className="signup-imgbox">
+                <div className="signup-img1">
+                  <img src="images/signup-grid1.jpg" alt="IWEA-IMG" />
+                </div>
+                <div className="signup-img2">
+                  <img src="images/signup-grid2.jpg" alt="IWEA-IMG" />
+                </div>
+                <div className="signup-img3">
+                  <img src="images/signup-grid3.jpg" alt="IWEA-IMG" />
+                </div>
+                <div className="signup-img4">
+                  <img src="images/signup-grid4.jpg" alt="IWEA-IMG" />
+                </div>
+                <div className="signup-img5">
+                  <img src="images/signup-grid5.jpg" alt="IWEA-IMG" />
+                </div>
+                <div className="signup-img6">
+                  <img src="images/signup-grid6.jpg" alt="IWEA-IMG" />
+                </div>
+                <div className="signup-img7">
+                  <img src="images/signup-grid7.jpg" alt="IWEA-IMG" />
+                </div>
+                <div className="signup-img8">
+                  <img src="images/signup-grid8.jpg" alt="IWEA-IMG" />
+                </div>
+                <div className="signup-img9"></div>
+                <div className="signup-img10">
+                  <img src="images/signup-grid9.jpg" alt="IWEA-IMG" />
+                </div>
+              </div>
             </div>
-            <div className="login-footer">
-              <div className="login-footer-inner">
-                <div className="login-footer-description">
+            <div className="space"></div>
+            <div className="signup-footer">
+              <div className="signup-footer-inner">
+                <div className="signup-footer-description">
                   <span>IWEA.kr - </span>
-                  <a target="_blank" href="#" className="login-cookie">
+                  <a target="_blank" href="#" className="signup-cookie">
                     쿠키 정책
                   </a>
                   <span> , </span>
-                  <a target="_blank" href="#" className="login-personal">
+                  <a target="_blank" href="#" className="signup-personal">
                     개인정보처리방침
                   </a>
                 </div>
-                <div className="login-footer-since">
+                <div className="signup-footer-since">
                   © Inter IWEA Systems B.V. 1999-2023
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="login-right">
-          <div className="login-right-inner">
+        <div className="signup-right">
+          <div className="signup-right-inner">
+            <p className="signup-welcome">IWEA에 가입하시겠어요?</p>
             <form className="form">
               <div className="form-inner">
-                <span>이메일 또는 확인된 휴대폰 번호</span>
-                <input type="text" id="username"></input>
-                <span className="login-option">
-                  다른 로그인 옵션: 일회용 코드로 로그인
-                </span>
-                <div className="email-space"></div>
-                <span class="password-name">비밀번호</span>
-                <input type="password" id="password"></input>
-                <span>비밀번호 찾기</span>
-                <div className="password-space-down"></div>
-                <div className="login-status">
-                  <input type="checkbox" className="login-checkbox"></input>
-                  <span className="login-checkbox-label">로그인 상태 유지</span>
-                  <img
-                    src="images/alert.png"
-                    alt="alert"
-                    className="login-alert"
-                  />
+                <span className="name-title">이름</span>
+                <input
+                  id="name"
+                  type="text"
+                  className="input-box"
+                  onChange={handleNameInput}
+                ></input>
+                <div className="space"></div>
+                <span className="name-title">이메일</span>
+                <input
+                  id="email"
+                  type="text"
+                  className="input-box"
+                  onChange={handleEmailInput}
+                ></input>
+                <div className="space"></div>
+                <span className="name-title">비밀번호</span>
+                <input
+                  id="password"
+                  type="password"
+                  className="input-box"
+                  onChange={handlePwInput}
+                ></input>
+                <div className="space"></div>
+                <span className="name-title">휴대폰</span>
+                <input
+                  id="phone"
+                  type="text"
+                  className="input-box"
+                  onChange={handlePhoneInput}
+                ></input>
+                <div className="space"></div>
+                <button class="address-button">우편번호 찾기</button>
+                <div className="space"></div>
+                <span className="name-title">주소</span>
+                <input
+                  id="address"
+                  type="text"
+                  className="input-box"
+                  onChange={handleAddressInput}
+                ></input>
+                <div className="space"></div>
+                <div class="checkbox">
+                  <input type="checkbox" className="input-checkbox"></input>
+                  <div className="input-description">
+                    마케팅 수신 동의를 통해 IKEA의 홈퍼니싱 아이디어와 신상품
+                    소식, 그리고 할인 혜택 정보를 받고 싶어요!
+                  </div>
                 </div>
-                <div className="login-space-up"></div>
-                <button class="login-button">로그인</button>
+                <div class="checkbox">
+                  <input type="checkbox" className="input-checkbox"></input>
+                  <div>
+                    (필수) 약관을 모두 읽고 동의합니다.
+                    <a target="_blank" href="#" class="link">
+                      이용약관
+                    </a>
+                    .
+                  </div>
+                </div>
+                <div class="checkbox">
+                  <input type="checkbox" className="input-checkbox"></input>
+                  <div>
+                    (필수) 개인정보 수집ㆍ이용에 동의합니다.{' '}
+                    <a target="_blank" href="#" class="link">
+                      개인정보 수집ㆍ이용 동의
+                    </a>
+                    .
+                  </div>
+                </div>
+                <div className="space"></div>
+                <button className="signup-button">회원 가입</button>
               </div>
             </form>
-            <div className="footer-space-up"></div>
-            <div className="login-right-footer">
-              <div className="right-footer-inner">
-                <span>IWEA 계정이 없으신가요? 지금 바로 만들어보세요.</span>
-                <div className="footer-space"></div>
-                <button>개인 회원 가입하기</button>
-              </div>
-            </div>
           </div>
         </div>
       </div>
