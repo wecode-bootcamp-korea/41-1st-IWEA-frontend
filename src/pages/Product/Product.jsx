bd401e27e525a1b374fbf7b4a1fffccb89aaca13
jakesungjaekim <codejakecarry@gmail.com>
2023-01-03 16:33:25 +0900

    ADD: productPage Mock.data가져오기, input filter추가

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,28 +1,55 @@
-import React, { useState, useEffect } from 'react';
-// import './Product.scss';
+import React, { useState, useEffect, useRef } from 'react';
+import './Product.scss';
 
 import Carousel from '../../components/Carousel';
 import ProductList from './components/ProductList';
 
 const Product = () => {
   const [productList, setProductList] = useState([]);
+  const [searchProduct, setSearchProduct] = useState('');
+
+  const options = {
+    threshold: 1.0,
+  };
+  const callback = () => {
+    console.log('관측되었습니다.');
+  };
+  const observer = new IntersectionObserver(callback, options);
+  const target = useRef(null);
+
+  const handleSearchProduct = e => {
+    setSearchProduct(e.target.value);
+  };
+
+  const searchedProduct = productList.filter(product => {
+    return product.name.toLowerCase().includes(searchProduct.toLowerCase());
+  });
 
   useEffect(() => {
     fetch('/data/mock.json')
       .then(response => response.json())
       .then(data => setProductList(data));
   }, []);
-  console.log(productList);
+  // console.log(productList);
+
+  useEffect(() => {
+    observer.observe(target.current);
+  }, []);
 
   return (
     <div className="Product">
       <div className="product-carousel-wrap">
         <Carousel />
       </div>
-      <div className="product-lists-container">
-        <input />
+      <div className="product-lists-container" ref={target}>
+        <input
+          className="product-search"
+          value={searchProduct}
+          onChange={handleSearchProduct}
+          placeholder="찾으실 가구이름을 입력해주세요"
+        />
         <div className="line"></div>
-        <ProductList productList={productList} />
+        <ProductList productList={searchedProduct} />
       </div>
     </div>
   );
