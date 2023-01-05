c3cc0339036af025cf5c7ec67e4108b591edebab
jakesungjaekim <codejakecarry@gmail.com>
2023-01-05 13:15:43 +0900

    Modify: ProductItem 컴포넌트 삭제 및 ProductList페이지 무한스크롤 관련 코드 삭제

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,22 +1,12 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Product.scss';
 
 import Carousel from '../../components/Carousel';
 import ProductList from './components/ProductList';
 import ProductSort from './components/ProductSort';
-import { useParams, useSearchParams } from 'react-router-dom';
 
 const Product = () => {
   const [productList, setProductList] = useState([]);
-
-  const options = {
-    threshold: 1.0,
-  };
-  const callback = () => {
-    console.log('관측되었습니다.');
-  };
-  const observer = new IntersectionObserver(callback, options);
-  const target = useRef(null);
 
   useEffect(() => {
     fetch('http://10.58.52.92:3000/products', {
@@ -29,18 +19,12 @@ const Product = () => {
       .then(data => setProductList(data));
   }, []);
 
-  console.log(productList);
-
-  useEffect(() => {
-    observer.observe(target.current);
-  }, []);
-
   return (
     <div className="Product">
       <div className="product-carousel-wrap">
         <Carousel />
       </div>
-      <div className="product-lists-container" ref={target}>
+      <div className="product-lists-container">
         <input
           className="product-search"
           placeholder="찾으실 가구이름을 입력해주세요"
