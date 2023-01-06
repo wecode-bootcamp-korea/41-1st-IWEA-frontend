9e082e943700c3abf388b8ee0b1ad5bf1052d95a
jakesungjaekim <codejakecarry@gmail.com>
2023-01-06 20:59:43 +0900

    Fix: 홈 브랜치 관련 컴포넌트 삭제

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,22 +1,7 @@
 import React from 'react';
-// import CarouselTest from '../../components/Carousel';
-import Header from './components/Header';
-import HomeGrid from './components/HomeGrid';
-import HomeMain from './components/HomeMain';
 
 const Home = () => {
-  return (
-    <div className="Home">
-      {/* 헤더 - src컴포넌트 */}
-      <Header />
-      {/* 메인 - 레이아웃 */}
-      <HomeMain />
-      {/* <CarouselTest /> */}
-      <HomeGrid />
-
-      {/* 푸터 - src컴포넌트*/}
-    </div>
-  );
+  return <div className="Home">홈</div>;
 };
 
 export default Home;
