1cc7cc120582b1b40208ee7f8ee7ed0fa35dbeb5
ImGalim <dev.galim@gmail.com>
2023-01-10 15:30:40 +0900

    Modify : 동적 라우팅 구현

--- src/pages/ProductDetail/ProductDetail.jsx
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import ProductItem from './component/ProductItem/ProductItem';
 import ProductAside from './component/ProductAside/ProductAside';
 import ProductSlide from './component/ProductSlide/ProductSlide';
@@ -6,47 +7,46 @@ import './ProductDetail.scss';
 
 const ProductDetail = () => {
   const [product, setProduct] = useState({});
-  const [slideItemList, setSlideItemList] = useState({});
-  // const [product, setProduct] = useState(MOCK_DATA.data);
+  const [slideItemList, setSlideItemList] = useState([]);
+  const params = useParams();
+  const productId = params.id;
 
-  useEffect(() => {
-    fetch('http://10.58.52.155:3000/products/productId/10', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-      },
-    })
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        // console.log(data.data);
-        setProduct(data.data);
-        // console.log(product);
-      });
-  }, []);
-  console.log(product);
+  // useEffect(() => {
+  //   fetch(`http://10.58.52.155:3000/products/${productId}`, {
+  //     method: 'GET',
+  //     headers: {
+  //       'Content-Type': 'application/json;charset=utf-8',
+  //       Authorization: localStorage.getItem('TOKEN'),
+  //     },
+  //   })
+  //     .then(res => {
+  //       return res.json();
+  //     })
+  //     .then(data => {
+  //       setProduct(data.data);
+  //       // console.log(data);
+  //     });
+  // }, [productId]);
 
-  useEffect(() => {
-    fetch('http://10.58.52.155:3000/products', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-      },
-    })
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        // console.log(data.data);
-        setSlideItemList(data.data);
-      });
-  }, []);
-  console.log(slideItemList);
+  // useEffect(() => {
+  //   fetch('http://10.58.52.155:3000/products', {
+  //     method: 'GET',
+  //     headers: {
+  //       'Content-Type': 'application/json;charset=utf-8',
+  //       Authorization: localStorage.getItem('TOKEN'),
+  //     },
+  //   })
+  //     .then(res => {
+  //       return res.json();
+  //     })
+  //     .then(data => {
+  //       setSlideItemList(data.data);
+  //     });
+  // }, []);
 
   return (
     <div className="product-wrap">
-      {product.id && (
+      {product.id ? (
         <div className="product-content">
           <div className="product-main">
             <ProductItem
@@ -65,33 +65,10 @@ const ProductDetail = () => {
             <ProductSlide slideItemList={slideItemList} />
           </div>
         </div>
+      ) : (
+        <>상품이 없습니다.</>
       )}
     </div>
   );
 };
 export default ProductDetail;
-
-// const MOCK_DATA = {
-//   data: {
-//     id: 10,
-//     korean_name: '제이크',
-//     english_name: 'JJAKE',
-//     short_description: '100% 순면 소재의 보송보송한 이불',
-//     long_description: 'IWEA의 시그니처 침구 상품인 제이크.',
-//     price: '39900.000',
-//     image_url: [
-//       {
-//         url: 'https://user-images.githubusercontent.com/110670796/210497180-2228c9c6-4551-4936-905f-0b473b14b0c1.png',
-//       },
-//       {
-//         url: 'https://user-images.githubusercontent.com/110670796/210497180-2228c9c6-4551-4936-905f-0b473b14b0c1.png',
-//       },
-//       {
-//         url: 'https://user-images.githubusercontent.com/110670796/210497180-2228c9c6-4551-4936-905f-0b473b14b0c1.png',
-//       },
-//       {
-//         url: 'https://user-images.githubusercontent.com/110670796/210497180-2228c9c6-4551-4936-905f-0b473b14b0c1.png',
-//       },
-//     ],
-//   },
-// };
