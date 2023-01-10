b70288fdb627c2a53fd5269afba5b13e619fb417
jakesungjaekim <codejakecarry@gmail.com>
2023-01-10 18:26:32 +0900

    Modify: Conflict 수정중..

--- src/pages/ProductDetail/ProductDetail.jsx
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { useEffect } from 'react';
-import { useState } from 'react';
-
-const ProductDetail = () => {
-  const [item, setItem] = useState([]);
-  const params = useParams();
-  const itemId = params.id;
-
-  useEffect(() => {
-    fetch(`http://10.58.52.92:3000/products/productId/${itemId}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then(response => response.json())
-      .then(data => setItem(data.data));
-  }, []);
-
-  console.log('item 배열 값 확인', item);
-  console.log('itemId 값 확인', itemId);
-
-  return (
-    <div>
-      ProductDetail
-      <h2>{item.english_name}</h2>
-      <h2>{item.price}</h2>
-    </div>
-  );
-};
-=======
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import ProductItem from './component/ProductItem/ProductItem';
@@ -44,7 +10,6 @@ const ProductDetail = () => {
   const [slideItemList, setSlideItemList] = useState([]);
   const params = useParams();
   const productId = params.id;
->>>>>>> main
 
   useEffect(() => {
     fetch(`http://10.58.52.155:3000/products/${productId}`, {
