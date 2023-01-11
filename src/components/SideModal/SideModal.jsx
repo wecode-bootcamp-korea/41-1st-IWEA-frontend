6d69078a97dfc39b912874da44dfbdb752e4605e
ImGalim <dev.galim@gmail.com>
2023-01-11 19:03:20 +0900

    Modify : 주석 제거 및 scss 코드 수정

--- src/components/SideModal/SideModal.jsx
+++ src/components/SideModal/SideModal.jsx
@@ -7,21 +7,21 @@ const SideModal = ({ setSideBarMenu }) => {
   const [unmount, setUnmount] = useState(false);
   const [modal, setModal] = useState({});
 
-  // useEffect(() => {
-  //   fetch('http://10.58.52.56:3000/userInfo', {
-  //     method: 'GET',
-  //     headers: {
-  //       'Content-Type': 'application/json;charset=utf-8',
-  //       Authorization: localStorage.getItem('TOKEN'),
-  //     },
-  //   })
-  //     .then(res => {
-  //       return res.json();
-  //     })
-  //     .then(data => {
-  //       setModal(data.data);
-  //     });
-  // }, []);
+  useEffect(() => {
+    fetch('http://10.58.52.56:3000/userInfo', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        Authorization: localStorage.getItem('TOKEN'),
+      },
+    })
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setModal(data.data);
+      });
+  }, []);
 
   const closeModal = () => {
     setUnmount(true);
@@ -63,6 +63,7 @@ const SideModal = ({ setSideBarMenu }) => {
             )}
             <div className="sidemodal-list-wrap">
               <SideModalList />
+              <img src="/images/logo.png" alt="iwea 로고" />
             </div>
           </div>
         </div>
