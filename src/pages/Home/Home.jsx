804a20f76f153f0db89df54922fff5857c9223a1
jakesungjaekim <codejakecarry@gmail.com>
2023-01-11 14:58:47 +0900

    Modify: 리뷰반영

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import HomeEvent from './components/HomeEvent';
 import HomeGrid from './components/HomeGrid';
 import HomeMain from './components/HomeMain';
 import HomeEventCard from './components/HomeEventCard';
@@ -8,16 +7,12 @@ import HomeBanner from './components/HomeBanner';
 const Home = () => {
   return (
     <div className="Home">
-      {/* 헤더 - src컴포넌트 */}
+      <HomeBanner />
       <HomeMain />
       <HomeEventCard />
       <HomeGrid />
-      {/*<HomeEvent />*/}
-      {/* 푸터 - src컴포넌트*/}
     </div>
   );
 };
 
 export default Home;
-
-// rebase checking...
