f9f927ebf928f16419c265fc03baa5fc372cd133
gimcheolho <sm708311@gmail.com>
2023-01-11 17:16:07 +0900

    Modify: animation + transition의 두가지 관리포인트를 animtaion의 하나의 관리포인트로 변경

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Header from './components/Header';
 import HomeEvent from './components/HomeEvent';
 import HomeGrid from './components/HomeGrid';
 import HomeMain from './components/HomeMain';
@@ -9,7 +8,6 @@ const Home = () => {
   return (
     <div className="Home">
       {/* 헤더 - src컴포넌트 */}
-      <Header />
       <HomeMain />
       <Carousel />
       <HomeGrid />
