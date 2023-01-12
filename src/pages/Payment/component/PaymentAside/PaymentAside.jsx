6d20b21b12fc002ced6d5e799721df2b3dfa4c08
ImGalim <dev.galim@gmail.com>
2023-01-12 17:17:56 +0900

    Modify : side창 position 수정

--- src/pages/Payment/component/PaymentAside/PaymentAside.jsx
+++ src/pages/Payment/component/PaymentAside/PaymentAside.jsx
@@ -5,7 +5,9 @@ const PaymentAside = () => {
   return (
     <div className="wrap">
       <div className="payment-aside">
-        <h1 className="payment-aside-title">구매 내역</h1>
+        <div className="payment-wrap">
+          <h1 className="payment-aside-title">구매 내역</h1>
+        </div>
         <div>
           <p>고객님의 주문 내역을 조회해보세요.</p>
           <p>주문 확인 및 취소도 가능합니다.</p>
