229a901403f9c285b85471040e4834f45231e5b1
jakesungjaekim <codejakecarry@gmail.com>
2023-01-13 16:53:46 +0900

    Modify : Final

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import HomeGrid from './components/HomeGrid';
 import HomeEventCard from './components/HomeEventCard';
 import HomeBanner from './components/HomeBanner';
+import HomeMain from './components/HomeMain';
 
 const Home = () => {
   return (
     <div className="Home">
       <HomeBanner />
+      <HomeMain />
       <HomeEventCard />
       <HomeGrid />
     </div>
